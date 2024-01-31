2573688eb05322b7a6b11b3b01beed96d3782ba0
Mathieu Deschamps <mathieu.deschamps@loginline.com>
2024-01-31 11:26:05 +0100

    Fix permission error on non-admin users

--- visence/static/src/js/init_visence.js
+++ visence/static/src/js/init_visence.js
@@ -17,41 +17,33 @@ odoo.define('visence', ['web.session', 'web.rpc'], function (require) {
     rpc.query({
       model: 'res.users',
       method: 'read',
-      args: [[user[0]], ['name', 'email', 'company_id', 'phone', 'create_date']],
+      args: [[user[0]], ['name', 'email', 'company_id', 'phone', 'create_date', 'visence_apikey']],
     }).then(function (result) {
       if (result.length === 1) {
-        var { id: idExt, name, email, company_id: company, phone, create_date } = result[0];
+        var { id: idExt, name, email, company_id: company, phone, create_date, visence_apikey } = result[0];
         var [ company_id, company_name ] = company;
         var { firstName, lastName } = getNames(name);
 
-        rpc.query({
-          model: 'ir.config_parameter',
-          method: 'get_param',
-          args: ['visence.api_key'],
-        }).then(function (api_key) {
-          if (api_key && window.Visence) {
-            window.Visence.init({
-              apiKey: api_key,
-              endUser: {
-                idExt: idExt.toString(),
-                firstName,
-                lastName,
-                email,
-                phone: phone || '',
-                registerDate: new Date(create_date),
-                custom: {
-                  environment: window.location.origin, 
-                }
-              },
-              client: {
-                idExt: company_id.toString(),
-                name: company_name,
-              },
-            });
-          }
-        }).catch(function (error) {
-          console.error({ error });
-        });
+        if (visence_apikey && window.Visence) {
+          window.Visence.init({
+            apiKey: visence_apikey,
+            endUser: {
+              idExt: idExt.toString() + '-' + window.location.origin,
+              firstName,
+              lastName,
+              email,
+              phone: phone || '',
+              registerDate: new Date(create_date),
+              custom: {
+                environment: window.location.origin, 
+              }
+            },
+            client: {
+              idExt: company_id.toString(),
+              name: company_name,
+            },
+          });
+        }
       }
     }).catch(function (error) {
       console.error({ error }); 
