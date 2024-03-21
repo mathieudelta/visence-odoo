d587f4096f4522eacb18f9975c5bb26a77eaec6d
Mathieu Deschamps <mathieu.deschamps@loginline.com>
2024-03-21 11:01:55 +0100

    Divide models in files

--- visence/models/__init__.py
+++ visence/models/__init__.py
@@ -1,15 +1,2 @@
-from odoo import fields, models
-
-class ResConfigSettings(models.TransientModel):
-  _inherit = 'res.config.settings'
-
-  visence_apikey = fields.Char("Clé d'API Visence", config_parameter="visence.api_key")
-
-class ResUsers(models.Model):
-  _inherit = 'res.users'
-
-  visence_apikey = fields.Char("Clé d'API Visence", compute='_compute_visence_apikey')
-
-  def _compute_visence_apikey(self):
-    for user in self:
-      user.visence_apikey = self.env['ir.config_parameter'].sudo().get_param('visence.api_key')
+from . import res_config_settings
+from . import res_users
---
+++ visence/models/res_users.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+class ResUsers(models.Model):
+  _inherit = 'res.users'
+
+  visence_apikey = fields.Char("Clé d'API Visence", compute='_compute_visence_apikey')
+
+  def _compute_visence_apikey(self):
+    for user in self:
+      user.visence_apikey = self.env['ir.config_parameter'].sudo().get_param('visence.api_key')
